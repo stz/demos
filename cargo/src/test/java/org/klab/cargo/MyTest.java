e8551399740a599aa8d7a79ee2a91b8d156d8c1e
Olivier Lecrivain <olivier.lecrivain@gmail.com>
2009-04-08 09:37:58 +0000

    abandon...

---
+++ cargo/src/test/java/org/klab/cargo/MyTest.java
@@ -0,0 +1,13 @@
+package org.klab.cargo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyTest {
+    
+    @Test
+    public void myTest() {
+        Assert.assertTrue(true);
+        System.out.println("MyTest.myTest()");
+    }
+}
