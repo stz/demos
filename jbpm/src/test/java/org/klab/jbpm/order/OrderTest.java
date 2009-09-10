f80295377a046d765ef17e5f6010bc60989f2b31
Olivier Lecrivain <olivier.lecrivain@gmail.com>
2009-09-10 20:36:16 +0000

    Logs ok Complétion de tâche

--- jbpm/src/test/java/org/klab/jbpm/order/OrderTest.java
+++ jbpm/src/test/java/org/klab/jbpm/order/OrderTest.java
@@ -1,6 +1,7 @@
 package org.klab.jbpm.order;
 
 import org.jbpm.api.ProcessInstance;
+import org.jbpm.api.task.Task;
 import org.jbpm.test.JbpmTestCase;
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,16 +14,43 @@ public class OrderTest extends JbpmTestCase {
 	        .addResourceFromClasspath("org/klab/jbpm/order/order.jpdl.xml")
 	        .deploy();
 
+	    // instanciation d'un nouveau process
 	    ProcessInstance processInstance = executionService.startProcessInstanceByKey("order");
 	    
+	    // la première activité = paiement
 	    Assert.assertNotNull(processInstance);
 	    Assert.assertTrue(processInstance.isActive("paiement"));
 	    
-	    String paiementId = processInstance.findActiveExecutionIn("paiement").getId();
-	    processInstance = executionService.signalExecutionById(paiementId);
+	    log(processInstance.getId());
 	    
+	    // execution de la tâche
+	    String taskId = taskService.findPersonalTasks("client").get(0).getId();
+	    //taskService.takeTask(taskId, "client");
+	    taskService.completeTask(taskId);
+	    
+	    // envoi d'un signal au paiement
+	    String paiementId = processInstance.findActiveExecutionIn("paiement").getId();
+	    executionService.signalExecutionById(paiementId);
+
+	    log(processInstance.getId());
+	    
+	    // l'activité courante doit etre = activation
+	    processInstance = executionService.findProcessInstanceById(processInstance.getId());
 	    Assert.assertTrue(processInstance.isActive("activation"));
 	    
 	    repositoryService.deleteDeploymentCascade(deploymentId);
+	}
+	
+	private void log(String processId) {
+		System.out.println("--------");
+		ProcessInstance processInstance = executionService.findProcessInstanceById(processId);
+		System.out.println("Activities :");
+		for (String name : processInstance.findActiveActivityNames()) {
+			System.out.println("  " + name);
+		}
+		System.out.println("Client tasks :");
+		for (Task task : taskService.findPersonalTasks("client")) {
+			System.out.println("  " + task.getName() + " (" + task.getId() + ")");
+		}
 	}
 }
