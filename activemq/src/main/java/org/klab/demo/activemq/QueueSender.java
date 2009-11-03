f423fbe88706615364d2915667d93a1a4739ec8a
Olivier Lecrivain <olivier.lecrivain@gmail.com>
2009-11-03 10:51:58 +0000

    fichier properties oublié

--- activemq/src/main/java/org/klab/demo/activemq/QueueSender.java
+++ activemq/src/main/java/org/klab/demo/activemq/QueueSender.java
@@ -1,5 +1,7 @@
 package org.klab.demo.activemq;
 
+import javax.jms.Destination;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
@@ -9,10 +11,13 @@ public class QueueSender {
 
 	@Autowired
     private JmsTemplate jmsTemplate;
+
+	@Autowired
+    private Destination destination;
 	
     public void send(String msg) {
     	for (int i = 0; i < 100; i++) {
-    		jmsTemplate.convertAndSend("Queue.Name", msg + " " + i);
+    		jmsTemplate.convertAndSend(destination, msg + " " + i);
 		}
     }
 }
