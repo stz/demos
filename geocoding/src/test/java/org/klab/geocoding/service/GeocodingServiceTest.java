f7077178adc0e114997f7ee43f070aa056d2d7d7
Olivier Lecrivain <olivier.lecrivain@gmail.com>
2010-06-02 18:16:28 +0200

    Format

--- geocoding/src/test/java/org/klab/geocoding/service/GeocodingServiceTest.java
+++ geocoding/src/test/java/org/klab/geocoding/service/GeocodingServiceTest.java
@@ -2,24 +2,19 @@ package org.klab.geocoding.service;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.klab.geocoding.model.Coordinates;
+import org.klab.geocoding.service.impl.GoogleGeocodingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
-    "/geocoding-applicationContext.xml",
-    "/geocoding-test-applicationContext.xml"
-})
 public class GeocodingServiceTest {
-
+    
     @Autowired
     private GeocodingService geocodingService;
     
     @Test
     public void testGeocodeOK() throws GeocodingServiceException {
+        GeocodingService geocodingService = new GoogleGeocodingServiceImpl();
+        
         String address = "12 rue Tiquetonne, Paris, France";
         Coordinates coordinates = geocodingService.geocode(address);
         
@@ -31,8 +26,9 @@ public class GeocodingServiceTest {
     @Test
     public void testGeocodeKO() throws GeocodingServiceException {
         try {
-        	geocodingService.geocode("rue du aaa, aaa");
-        	Assert.fail();
-		} catch (GeocodingServiceException e) {}
+            geocodingService.geocode("rue du aaa, aaa");
+            Assert.fail();
+        } catch (GeocodingServiceException e) {
+        }
     }
 }
