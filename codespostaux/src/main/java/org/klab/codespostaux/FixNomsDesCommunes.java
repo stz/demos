5bc1f24e0bee62a51cb62a44c6797ed5d35f39bd
Olivier Lecrivain <olivier.lecrivain@sfr.com>
2010-08-02 16:58:36 +0200

    Ajout du module codespostaux

---
+++ codespostaux/src/main/java/org/klab/codespostaux/FixNomsDesCommunes.java
@@ -0,0 +1,11 @@
+package org.klab.codespostaux;
+
+/**
+ * Réécrit les noms des communes une orthographe correcte (accents, majuscules, minuscules...) 
+ */
+public class FixNomsDesCommunes {
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ codespostaux/src/main/resources/README.txt
@@ -0,0 +1,3 @@
+Le fichier cp.txt provient de http://www.keliglia.com/categorie/php/1986/.
+
+wget http://www.keliglia.com/data/codes_postaux_liste.txt
