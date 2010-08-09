19a519be45a22fd0ce08c83bc018e03159845149
Olivier Lecrivain <olivier.lecrivain@sfr.com>
2010-08-09 16:47:39 +0200

    Exemple de log logback

--- spring-mvc/src/main/java/org/klab/demo/springmvc/Application.java
+++ spring-mvc/src/main/java/org/klab/demo/springmvc/Application.java
@@ -1,14 +1,19 @@
 package org.klab.demo.springmvc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class ApplicationController {
+public class Application {
+    
+    private static Logger LOG = LoggerFactory.getLogger(Application.class);
     
     @RequestMapping("/home")
     public @ResponseBody String home() {
+        LOG.info("Hello world!");
         return "Hello world!";
     }
 }
